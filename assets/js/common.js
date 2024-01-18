9fe7de6119f2e003fbbce78a44558c6ae5f1d3bf
PasiniSamuele <samu.pasini98@gmail.com>
2024-01-18 15:24:54 +0100

    toggle

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,10 +3,20 @@ $(document).ready(function () {
   $("a.abstract").click(function () {
     $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".mla.hidden.open").toggleClass("open");
+
   });
   $("a.bibtex").click(function () {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".mla.hidden.open").toggleClass("open");
+
+  });
+  $("a.mla").click(function () {
+    $(this).parent().parent().find(".mla.hidden").toggleClass("open");
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+
   });
   $("a").removeClass("waves-effect waves-light");
 
